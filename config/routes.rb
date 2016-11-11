8a62b3703965fe4c0e58f89931ad5d8e30acaa00
Allan Burns <allanlburns@yahoo.com>
2016-11-11 02:52:26 +0000

    Create users/show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'contact' =>  'static_pages#contact'
   
   get 'signup'  =>  'users#new'
+  
+  resources :users
 
 
   # The priority is based upon order of0144 creation: first created -> highest priority.
