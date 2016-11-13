ffb6e69475b5d6474375b928d5805c37c62b5d10
Allan Burns <allanlburns@yahoo.com>
2016-11-13 23:50:04 +0000

    Add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get     'sessions/new'
 
-  root 'static_pages#home'
+  get     'users/new'
 
-  get 'help'    =>  'static_pages#help'
+  root    'static_pages#home'
+
+  get     'help'    =>  'static_pages#help'
   
-  get 'about'   =>  'static_pages#about'
+  get     'about'   =>  'static_pages#about'
   
-  get 'contact' =>  'static_pages#contact'
+  get     'contact' =>  'static_pages#contact'
   
-  get 'signup'  =>  'users#new'
+  get     'signup'  =>  'users#new'
+  
+  get     'login'   =>  'sessions#new'
+  
+  post    'login'   =>  'sessions#create'
+  
+  delete  'logout'  =>  'sessions#destroy'
   
   resources :users
 
