46f0657ee2612994726f1167439a5ca46e2b6f80
Allan Burns <allanlburns@yahoo.com>
2016-11-15 02:05:51 +0000

    Try sessions#new instead of login_path in header.html

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-     flash.now[:danger] = 'Invalid email/password combination'
-    render 'new'
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
   
