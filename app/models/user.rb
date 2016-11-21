c57b3025c308c6267867c2924f22b5e82ef19f29
Allan Burns <allanlburns@yahoo.com>
2016-11-21 02:27:09 +0000

    Elseif is spelled elsif in Ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
+  attr_accessor :remember_me # found this on Stack Overflow http://stackoverflow.com/questions/33385228/undefined-method-remember-me-in-nested-form-for
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
