7fe8882262031d1f5403daf31b2f3dc7520eab1a
Allan Burns <allanlburns@yahoo.com>
2016-12-12 01:48:40 +0000

    change carrier wave from actual values to generic

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider                 => 'AWS',
-      :aws_access_key_id        => ENV['AKIAINI2RQ5BHVDOY7EA'],
-      :aws_secret_access_key    => ENV['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']
+      :aws_access_key_id        => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key    => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory        = ENV['allan-bucket']
+    config.fog_directory        = ENV['S3_BUCKET']
   end
 end
