55b2e9eca5772987c093b5d85028a6ca1c77d273
Allan Burns <allanlburns@yahoo.com>
2016-12-12 00:11:08 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,18 @@ User.create!(name: "Example User",
                 activated: true,
                 activated_at: Time.zone.now)
 end
-                
+
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+following = users[3..40]
+following.each { |followed| user.follow(followed) }
+following.each { |follower| follower.follow(user) }
